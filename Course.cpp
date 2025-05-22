e2d13d5f898f9eaa65cb9f0a5f242f0209996c18
AchilleasRakovitis <axilrakovitis@gmail.com>
2025-05-23 01:36:07 +0300

    finished Course.h

---
+++ Course.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include "Course.h"
+
+using namespace std;
+
---
+++ Course.h
@@ -0,0 +1,28 @@
+#include <string>
+
+class Course{
+    // attributes
+private:
+    char * courseID;
+    std::string courseName;
+    int courseSemester;
+    std::string profName;
+
+    //methods
+public:
+    //constructors
+    Course(const char *, const std::string&, int, const std::string& );
+    Course(const Course& other); // copy constructor 
+    ~Course(); // destructor 
+    //setters
+    void setCourseID(const char * );
+    void setCourseName(const std::string& );
+    void setCourseSemester(int );
+    void setProfName(const std::string& );
+    //getters
+    const char * getCourseID() const;
+    const std::string& getCourseName() const;
+    int getCourseSemester() const;
+    const std::string& getProfName() const;
+
+};
